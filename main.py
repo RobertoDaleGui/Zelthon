2f2917dd6d3c96076deef1e24a10d20caf7eb274
Roberto D'Alessandro Guimarães <robertodalegui@gmail.com>
2022-05-03 20:01:27 -0300

    FEAT: Game title

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        pygame.display.set_caption('Zelthon')
         self.clock = pygame.time.Clock()
     
     def run(self):
