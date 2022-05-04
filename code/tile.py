c8638e76df6bebd4406a810ca2542035fd0c91af
Roberto D'Alessandro Guimarães <robertodalegui@gmail.com>
2022-05-03 22:56:54 -0300

    FEAT: mapa do jogo

--- code/tile.py
+++ code/tile.py
@@ -1,12 +1,9 @@
-from cgi import test
-import os
 import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
   def __init__(self, pos, groups):
     super().__init__(groups)
-    # self.image = pygame.image.load(os.path.join('graphics', 'test/rock.png')).convert_alpha()
     self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
     self.rect = self.image.get_rect(topleft = pos)
     self.hitbox = self.rect.inflate(0, -10)
