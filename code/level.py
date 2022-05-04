9bcd8ece0fbb16ec45ab71a876632ffdf35b245b
Roberto D'Alessandro Guimarães <robertodalegui@gmail.com>
2022-05-03 21:23:10 -0300

    FEAT: movimentação do personagem

--- code/level.py
+++ code/level.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
-
+from debug import debug
 class Level:
   def __init__(self):
     self.display_surface = pygame.display.get_surface()
@@ -20,6 +20,8 @@ class Level:
         if col == 'x':
           Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
         if col == 'p':
-          Player((x,y), [self.visible_sprites])
+          self.player = Player((x,y), [self.visible_sprites])
   def run(self):
     self.visible_sprites.draw(self.display_surface)
+    self.visible_sprites.update()
+    debug(self.player.direction)
